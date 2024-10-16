9db4777c7e9c643edffad93173799acb16b160d0
ionztorm <leon.codes@icloud.com>
2024-10-16 08:52:16 +0100

    refactor logo component to accept text props

--- src/components/generic/logo.tsx
+++ src/components/generic/logo.tsx
@@ -1,8 +1,12 @@
-export function Logo() {
-  return (
-    <h1 className="flex flex-col">
-      <span className="text-2xl font-bold">Leon Lonsdale</span>
-      <span>Learning through video</span>
-    </h1>
-  );
+type TLogoProps = {
+	title: string;
+	subtext?: string;
+};
+export function Logo({ title, subtext }: TLogoProps) {
+	return (
+		<h1 className="flex flex-col">
+			<span className="text-2xl font-bold">{title}</span>
+			{subtext && <span>{subtext}</span>}
+		</h1>
+	);
 }
