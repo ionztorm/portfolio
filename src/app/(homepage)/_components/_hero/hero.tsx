c5a0010f367d5ae2d13970a50cc75e90d03339e1
ionztorm <leon.codes@icloud.com>
2024-10-17 18:54:00 +0100

    initial hero setup

--- src/app/(homepage)/_components/_hero/hero.tsx
+++ src/app/(homepage)/_components/_hero/hero.tsx
@@ -1,3 +1,12 @@
 export function Hero() {
-	return <h1>Hero</h1>;
+	return (
+		<div className="h-[45vh] flex flex-col items-center justify-center gap-3">
+			<h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
+				I&apos;m Leon
+			</h1>
+			<p className="text-slate-500 text-center text-sm">
+				Fullstack Web Developer 💻. Tech Enthusiast 📱. Formula 1 Enjoyer 🏎️.
+			</p>
+		</div>
+	);
 }
