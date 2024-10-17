4ad009498f0b726ac6def10927fd7fbe4823a6f8
ionztorm <leon.codes@icloud.com>
2024-10-17 13:54:50 +0100

    add margin to sections

--- src/components/generic/section-heading.tsx
+++ src/components/generic/section-heading.tsx
@@ -4,7 +4,7 @@ type TSectionHeadingProps = {
 export function SectionHeading({ children }: TSectionHeadingProps) {
 	return (
 		<header>
-			<h2 className="text-3xl font-bold text-slate-800">{children}</h2>
+			<h2 className="text-3xl font-bold text-slate-800 mb-5">{children}</h2>
 		</header>
 	);
 }
