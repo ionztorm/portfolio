7126af749124b20d60784fa71d7360d83f8439a4
ionztorm <leon.codes@icloud.com>
2024-10-17 13:53:31 +0100

    add about placeholder text

--- src/app/(homepage)/_components/_about/about.tsx
+++ src/app/(homepage)/_components/_about/about.tsx
@@ -1,7 +1,22 @@
 export function About() {
-  return (
-    <article>
-      <p>A little something about me</p>
-    </article>
-  );
+	return (
+		<article className="grid gap-4">
+			<p>Hey, I&apos;m Leon, it&apos;s great to meet you...</p>
+			<p>
+				I&apos;m a self taught web developer hoping to secure employment as a
+				junior developer. I&apos;ve spent my time so far studying HTML, CSS,
+				JavaScript, TypeScript, TailwindCSS, React, and NextJS. I&apos;ve also
+				studied some GoLang, and have plans to learn PHP and Laravel.
+			</p>
+			<p>
+				When I&apos;m not studying, I enjoy watching Formula 1, and have a
+				collection of model F1 cars and helmets (15 cars and 3 helmets, and
+				counting). I like to keep myself active with either a 25km indoor cycle
+				or 6km outdoor walk every day, weather permitting.
+			</p>
+			<p>
+				While you&apos;re here, why not check out some of my projects below.
+			</p>
+		</article>
+	);
 }
