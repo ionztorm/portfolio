32a1e919ddf8924d5167ee76e56c91186473ca20
ionztorm <leon.codes@icloud.com>
2024-10-17 21:03:58 +0100

    stlye section headings

--- src/components/generic/section-heading.tsx
+++ src/components/generic/section-heading.tsx
@@ -4,7 +4,9 @@ type TSectionHeadingProps = {
 export function SectionHeading({ children }: TSectionHeadingProps) {
 	return (
 		<header>
-			<h2 className="text-3xl font-bold text-slate-800 mb-5">{children}</h2>
+			<h2 className="text-2xl text-center lg:text-left sm:text-4xl font-bold text-slate-800 mb-10">
+				{children}
+			</h2>
 		</header>
 	);
 }
