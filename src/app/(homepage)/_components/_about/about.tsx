a04bcf30d7987e407a7bb4e86b015fa2ae7da10f
ionztorm <leon.codes@icloud.com>
2024-10-17 21:02:00 +0100

    rewrite about page

--- src/app/(homepage)/_components/_about/about.tsx
+++ src/app/(homepage)/_components/_about/about.tsx
@@ -1,22 +1,28 @@
+import { SectionHeading } from "@/components/generic/section-heading";
+
 export function About() {
 	return (
-		<article className="grid gap-4">
-			<p>Hey, I&apos;m Leon, it&apos;s great to meet you...</p>
-			<p>
-				I&apos;m a self taught web developer hoping to secure employment as a
-				junior developer. I&apos;ve spent my time so far studying HTML, CSS,
-				JavaScript, TypeScript, TailwindCSS, React, and NextJS. I&apos;ve also
-				studied some GoLang, and have plans to learn PHP and Laravel.
-			</p>
-			<p>
-				When I&apos;m not studying, I enjoy watching Formula 1, and have a
-				collection of model F1 cars and helmets (15 cars and 3 helmets, and
-				counting). I like to keep myself active with either a 25km indoor cycle
-				or 6km outdoor walk every day, weather permitting.
-			</p>
-			<p>
-				While you&apos;re here, why not check out some of my projects below.
-			</p>
-		</article>
+		<>
+			<SectionHeading>Get to know me</SectionHeading>
+			<article className="grid gap-4">
+				<p>
+					Hey, I&apos;m Leon. I&apos;m a <strong>fullstack</strong> web
+					developer with a <strong>passion</strong> for <strong>modern</strong>{" "}
+					technologies and developing <strong>clean</strong>,{" "}
+					<strong>responsive user experiences</strong>.
+				</p>
+				<p>
+					I&apos;m self taught, which has allowed me to choose the technologies
+					I have a parrion for. Check out my skills below to see what I&apos;ve
+					been spending my time on.
+				</p>
+				<p>
+					When I&apos;m not studying, I enjoy watching Formula 1, and have a
+					collection of model F1 cars and helmets (15 cars and 3 helmets, and
+					counting). I like to keep myself active with either a 25km indoor
+					cycle or 6km outdoor walk every day, weather permitting.
+				</p>
+			</article>
+		</>
 	);
 }
