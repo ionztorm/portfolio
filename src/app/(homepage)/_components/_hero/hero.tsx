c50996c48e6c7da8837a5587de4a756ebe2254a3
ionztorm <leon.codes@icloud.com>
2024-10-17 21:02:21 +0100

    build hero section

--- src/app/(homepage)/_components/_hero/hero.tsx
+++ src/app/(homepage)/_components/_hero/hero.tsx
@@ -2,10 +2,13 @@ export function Hero() {
 	return (
 		<div className="h-[45vh] flex flex-col items-center justify-center gap-3">
 			<h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
-				I&apos;m Leon
+				I&apos;m{" "}
+				<span className="inline-block text-violet-500 transform transition hover:-rotate-6 hover:scale-105 duration-300">
+					Leon
+				</span>
 			</h1>
-			<p className="text-slate-500 text-center text-sm">
-				Fullstack Web Developer 💻. Tech Enthusiast 📱. Formula 1 Enjoyer 🏎️.
+			<p className="text-slate-500 text-center">
+				Fullstack Web Developer 💻. Formula 1 Enjoyer 🏎️. Lifelong Learner 🎓
 			</p>
 		</div>
 	);
