9b9428249396e3935a85ace0f182b481c65b160c
ionztorm <leon.codes@icloud.com>
2024-10-17 18:54:58 +0100

    change logo from h1 to p

--- src/components/generic/logo.tsx
+++ src/components/generic/logo.tsx
@@ -4,9 +4,9 @@ type TLogoProps = {
 };
 export function Logo({ title, subtext }: TLogoProps) {
 	return (
-		<h1 className="grid">
+		<p className="grid">
 			<span className="text-lg sm:text-2xl lg:text-3xl font-bold">{title}</span>
 			{subtext && <span>{subtext}</span>}
-		</h1>
+		</p>
 	);
 }
