5bb00862ab2bf4d553c9408ee2420828f3f3b918
ionztorm <leon.codes@icloud.com>
2024-10-17 13:56:12 +0100

    update constans path object to include skills and icons

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,8 @@
 import type { TPath } from "./types";
 
 export const HOMEPAGE_NAV_PATHS: TPath[] = [
-	{ path: "#about", label: "About" },
-	{ path: "#projects", label: "Projects" },
-	{ path: "#contact", label: "Contact" },
+	{ path: "#about", label: "About", icon: "" },
+	{ path: "#projects", label: "Projects", icon: "" },
+	{ path: "#skills", label: "Skills", icon: "" },
+	{ path: "#contact", label: "Contact", icon: "" },
 ];
