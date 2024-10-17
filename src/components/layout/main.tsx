1148c440a8a2919b9e5950ac6c45efb938987f2a
ionztorm <leon.codes@icloud.com>
2024-10-17 13:58:43 +0100

    allow main ele to receive classNames as props

--- src/components/layout/main.tsx
+++ src/components/layout/main.tsx
@@ -1,6 +1,13 @@
+import { Wrapper } from "../generic/wrapper";
+
 type TMainProps = {
-  children: React.ReactNode;
+	children: React.ReactNode;
+	className: string;
 };
-export function Main({ children }: TMainProps) {
-  return <main className="flex flex-col gap-10">{children}</main>;
+export function Main({ children, className }: TMainProps) {
+	return (
+		<main className={className}>
+			<Wrapper className="grid gap-10">{children}</Wrapper>
+		</main>
+	);
 }
