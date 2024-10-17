e97895bfeb1e2ef80b17ea87cad51f642590fd7d
ionztorm <leon.codes@icloud.com>
2024-10-17 13:58:00 +0100

    change nav link font slate-700

--- src/components/layout/nav-link.tsx
+++ src/components/layout/nav-link.tsx
@@ -2,15 +2,15 @@ import { cn } from "@/utils/cn";
 import Link from "next/link";
 
 type TNavLinkProps = {
-  children: React.ReactNode;
-  path: string;
-  className?: string;
+	children: React.ReactNode;
+	path: string;
+	className?: string;
 };
 
 export function NavLink({ children, path, className }: TNavLinkProps) {
-  return (
-    <Link className={cn("font-semibold", className)} href={path}>
-      {children}
-    </Link>
-  );
+	return (
+		<Link className={cn("text-slate-700", className)} href={path}>
+			{children}
+		</Link>
+	);
 }
