ffe38d41578df818a4f0f81734099c4f0eb32169
ionztorm <leon.codes@icloud.com>
2024-10-19 17:27:00 +0100

    adjust section heading font sizing

--- src/components/generic/section-heading.tsx
+++ src/components/generic/section-heading.tsx
@@ -3,7 +3,7 @@ import type { TSectionHeadingProps } from "@/lib/types";
 export function SectionHeading({ children }: TSectionHeadingProps) {
   return (
     <header>
-      <h2 className="text-2xl text-center lg:text-left sm:text-4xl font-bold text-slate-800 mb-10">
+      <h2 className="text-lg text-center lg:text-left font-semibold uppercase">
         {children}
       </h2>
     </header>
