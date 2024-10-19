a1bc85cfd9c607a511053eec2f79e73b44c85cff
ionztorm <leon.codes@icloud.com>
2024-10-19 19:37:21 +0100

    add social links

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,27 @@
 "use client";
-import { HOMEPAGE_NAV_PATHS } from "@/lib/constants";
-import { Navbar } from "./navbar";
-import { Logo } from "../generic/logo";
-import { Wrapper } from "../generic/wrapper";
+import { SOCIAL_LINKS } from "@/lib/constants";
 import type { THeaderProps } from "@/lib/types";
 import { Hero } from "@/app/(homepage)/_components/_hero/hero";
+import { StyledLink } from "../generic/styled-link";
 
 export function Header({ className }: THeaderProps) {
   return (
     <header className={className}>
       <Hero />
+      <ul className="flex gap-3">
+        <li>
+          <StyledLink path={SOCIAL_LINKS.github} />
+        </li>
+        <li>
+          <StyledLink path={SOCIAL_LINKS.X} />
+        </li>
+        <li>
+          <StyledLink path={SOCIAL_LINKS.instagram} />
+        </li>
+        <li>
+          <StyledLink path={SOCIAL_LINKS.discord} />
+        </li>
+      </ul>
       {/* 
         <Navbar paths={HOMEPAGE_NAV_PATHS}>
           <Logo title="Leon Lonsdale" />
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,3 +6,10 @@ export const HOMEPAGE_NAV_PATHS: TPaths = {
   skills: { path: "#skills", label: "Skills", icon: "" },
   contact: { path: "#contact", label: "Contact", icon: "" },
 };
+
+export const SOCIAL_LINKS: TPaths = {
+  github: { path: "https://github.com/ionztorm", label: "github", icon: "" },
+  X: { path: "", label: "X", icon: "" },
+  instagram: { path: "", label: "instagram", icon: "" },
+  discord: { path: "", label: "Discord", icon: "" },
+};
