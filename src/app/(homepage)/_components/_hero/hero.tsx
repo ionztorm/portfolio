f2ad24f2e4b9142d3d522a31529e87319c93f887
ionztorm <leon.codes@icloud.com>
2024-10-19 17:25:09 +0100

    update hero for 2 column layout

--- src/app/(homepage)/_components/_hero/hero.tsx
+++ src/app/(homepage)/_components/_hero/hero.tsx
@@ -1,15 +1,16 @@
 export function Hero() {
   return (
-    <div className="h-[45vh] flex flex-col items-center justify-center gap-3">
-      <h1 className="flex flex-col items-center justify-center gap-2 text-slate-400">
-        <div className="group grid">
-          <span className="text-4xl sm:text-5xl font-bold inline-block text-violet-500 transform transition  duration-300">
+    <div className="h-[45vh] flex flex-col gap-3">
+      <h1 className="flex flex-col justify-center gap-2 text-slate-400">
+        <span>Hey, the name is</span>
+        <div className="group grid max-w-max">
+          <span className="text-4xl md:text-5xl font-bold inline-block text-slate-200 transform transition  duration-300">
             Leon Lonsdale
           </span>
           {/* Underline Span */}
           <span className="h-1 bg-violet-500 w-0 transition-all duration-500 group-hover:w-full"></span>
         </div>
-        <span className="text-center text-lg sm:text-xl">
+        <span className="text-lg sm:text-xl">
           Aspiring Junior Fullstack Engineer
           {/*a Fullstack Web Developer 💻. Formula 1 Enjoyer 🏎. Lifelong Learner
           🎓*/}
