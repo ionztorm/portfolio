3546ee487a1d5d8c14120e79a93023bb4a31e97b
ionztorm <leon.codes@icloud.com>
2024-10-19 17:25:54 +0100

    add bg to homepage layout

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -1,6 +1,6 @@
 type THomepageLayoutProps = {
-	children: React.ReactNode;
+  children: React.ReactNode;
 };
 export default function HomepageLayout({ children }: THomepageLayoutProps) {
-	return <>{children}</>;
+  return <div className="bg-blue-950 text-white">{children}</div>;
 }
