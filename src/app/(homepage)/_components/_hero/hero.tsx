d16480803cea3176516bbb231457cda91bc3bfce
ionztorm <leon.codes@icloud.com>
2024-10-19 15:25:16 +0100

    change hero style and text

--- src/app/(homepage)/_components/_hero/hero.tsx
+++ src/app/(homepage)/_components/_hero/hero.tsx
@@ -1,15 +1,20 @@
 export function Hero() {
-	return (
-		<div className="h-[45vh] flex flex-col items-center justify-center gap-3">
-			<h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
-				I&apos;m{" "}
-				<span className="inline-block text-violet-500 transform transition hover:-rotate-6 hover:scale-105 duration-300">
-					Leon
-				</span>
-			</h1>
-			<p className="text-slate-500 text-center">
-				Fullstack Web Developer 💻. Formula 1 Enjoyer 🏎️. Lifelong Learner 🎓
-			</p>
-		</div>
-	);
+  return (
+    <div className="h-[45vh] flex flex-col items-center justify-center gap-3">
+      <h1 className="flex flex-col items-center justify-center gap-2 text-slate-400">
+        <div className="group grid">
+          <span className="text-4xl sm:text-5xl font-bold inline-block text-violet-500 transform transition  duration-300">
+            Leon Lonsdale
+          </span>
+          {/* Underline Span */}
+          <span className="h-1 bg-violet-500 w-0 transition-all duration-500 group-hover:w-full"></span>
+        </div>
+        <span className="text-center text-lg sm:text-xl">
+          Aspiring Junior Fullstack Engineer
+          {/*a Fullstack Web Developer 💻. Formula 1 Enjoyer 🏎. Lifelong Learner
+          🎓*/}
+        </span>
+      </h1>
+    </div>
+  );
 }
