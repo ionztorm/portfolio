072455d1e23b3920d0735de8d777e3e349d9beb7
ionztorm <leon.codes@icloud.com>
2024-10-19 17:27:31 +0100

    update header for 2 column layout

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,18 +4,19 @@ import { Navbar } from "./navbar";
 import { Logo } from "../generic/logo";
 import { Wrapper } from "../generic/wrapper";
 import type { THeaderProps } from "@/lib/types";
+import { Hero } from "@/app/(homepage)/_components/_hero/hero";
 
 export function Header({ className }: THeaderProps) {
   return (
     <header className={className}>
-      <Wrapper>
+      <Hero />
+      {/* 
         <Navbar paths={HOMEPAGE_NAV_PATHS}>
           <Logo title="Leon Lonsdale" />
         </Navbar>
-        {/* <div className="w-full aspect-video border border-dashed rounded-md flex items-center justify-center shadow-md overflow-hidden">
+        <div className="w-full aspect-video border border-dashed rounded-md flex items-center justify-center shadow-md overflow-hidden">
 				I&apos;m a photo
         </div> */}
-      </Wrapper>
     </header>
   );
 }
