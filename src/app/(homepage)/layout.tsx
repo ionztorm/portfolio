790a4a34549a505c1d1c08acaef3639431f7af6c
ionztorm <leon.codes@icloud.com>
2024-10-20 11:25:28 +0100

    move navbar into layout and hero into header

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -1,6 +1,21 @@
+"use client";
+import { Logo } from "@/components/generic/logo";
+import { Footer } from "@/components/layout/footer";
+import { Navbar } from "@/components/layout/navbar";
+import { HOMEPAGE_NAV_PATHS } from "@/lib/constants";
+
 type THomepageLayoutProps = {
 	children: React.ReactNode;
 };
 export default function HomepageLayout({ children }: THomepageLayoutProps) {
-	return <>{children}</>;
+	return (
+		<>
+			<Navbar paths={HOMEPAGE_NAV_PATHS}>
+				<Logo title="Leon Lonsdale" />
+			</Navbar>
+			{children}
+
+			<Footer />
+		</>
+	);
 }
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,21 +1,13 @@
-"use client";
-import { HOMEPAGE_NAV_PATHS } from "@/lib/constants";
-import { Navbar } from "./navbar";
-import { Logo } from "../generic/logo";
-import { Wrapper } from "../generic/wrapper";
+import { Hero } from "@/app/(homepage)/_components/_hero/hero";
 import type { THeaderProps } from "@/lib/types";
+import { Wrapper } from "../generic/wrapper";
 
 export function Header({ className }: THeaderProps) {
-  return (
-    <header className={className}>
-      <Wrapper>
-        <Navbar paths={HOMEPAGE_NAV_PATHS}>
-          <Logo title="Leon Lonsdale" />
-        </Navbar>
-        {/* <div className="w-full aspect-video border border-dashed rounded-md flex items-center justify-center shadow-md overflow-hidden">
-				I&apos;m a photo
-        </div> */}
-      </Wrapper>
-    </header>
-  );
+	return (
+		<header className={className}>
+			<Wrapper className="bg-white shadow-lg rounded-3xl py-10 sm:py-16 grid place-content-center place-items-center gap-3">
+				<Hero />
+			</Wrapper>
+		</header>
+	);
 }
