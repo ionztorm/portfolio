51b79a273a8ba412c1477e5fe7d5f847bd47a733
ionztorm <leon.codes@icloud.com>
2024-10-20 14:10:26 +0100

    create convex icon, include skills in skills.tsx and add to header

--- src/app/(homepage)/_components/_skills/skills.tsx
+++ src/app/(homepage)/_components/_skills/skills.tsx
@@ -1,12 +1,20 @@
 import { SectionHeading } from "@/components/generic/section-heading";
+import { SKILLS } from "@/lib/constants";
 
 export function Skills() {
-	return (
-		<>
-			<SectionHeading>Skills</SectionHeading>
-			<article>
-				<p>Skills</p>
-			</article>
-		</>
-	);
+  return (
+    <>
+      <ul className="mx-auto flex items-center justify-center gap-4 flex-wrap px-10 py-4">
+        {Object.values(SKILLS).map((skill) => (
+          <li
+            key={skill.name}
+            className="flex items-center gap-2 flex-shrink-0 border rounded-full px-3 py-1"
+          >
+            <span>{skill.icon}</span>
+            <span>{skill.name}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,16 @@
 import { Hero } from "@/app/(homepage)/_components/_hero/hero";
 import type { THeaderProps } from "@/lib/types";
 import { Wrapper } from "../generic/wrapper";
+import { ConvexIcon } from "../icon/convex";
+import { Skills } from "@/app/(homepage)/_components/_skills/skills";
 
 export function Header({ className }: THeaderProps) {
-	return (
-		<header className={className}>
-			<Wrapper className="bg-white shadow-lg rounded-3xl py-10 sm:py-16 grid place-content-center place-items-center gap-3">
-				<Hero />
-			</Wrapper>
-		</header>
-	);
+  return (
+    <header className={className}>
+      <Wrapper className="bg-white shadow-lg rounded-3xl py-10 sm:py-16 grid place-content-center place-items-center gap-3">
+        <Hero />
+        <Skills />
+      </Wrapper>
+    </header>
+  );
 }
