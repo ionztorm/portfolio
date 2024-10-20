a5d24d4e8116b5f21792b48a0554963d32bdb8cd
ionztorm <leon.codes@icloud.com>
2024-10-20 11:29:46 +0100

    rework hero section

--- src/app/(homepage)/_components/_hero/hero.tsx
+++ src/app/(homepage)/_components/_hero/hero.tsx
@@ -1,20 +1,13 @@
 export function Hero() {
-  return (
-    <div className="h-[45vh] flex flex-col items-center justify-center gap-3">
-      <h1 className="flex flex-col items-center justify-center gap-2 text-slate-400">
-        <div className="group grid">
-          <span className="text-4xl sm:text-5xl font-bold inline-block text-violet-500 transform transition  duration-300">
-            Leon Lonsdale
-          </span>
-          {/* Underline Span */}
-          <span className="h-1 bg-violet-500 w-0 transition-all duration-500 group-hover:w-full"></span>
-        </div>
-        <span className="text-center text-lg sm:text-xl">
-          Aspiring Junior Fullstack Engineer
-          {/*a Fullstack Web Developer 💻. Formula 1 Enjoyer 🏎. Lifelong Learner
-          🎓*/}
-        </span>
-      </h1>
-    </div>
-  );
+	return (
+		<h1 className="group grid gap-2 text-center">
+			<span className="text-center text-sm">Aspiring Junior</span>
+
+			<span className="text-3xl sm:text-5xl font-bold">
+				Fullstack Web Developer
+			</span>
+			{/* Underline Span */}
+			<span className="h-1 bg-violet-500 w-0 transition-all duration-500 group-hover:w-full" />
+		</h1>
+	);
 }
