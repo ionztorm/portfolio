0b8c5cb6962d899358baab52836b947d51dd0df0
ionztorm <leon.codes@icloud.com>
2024-10-20 17:49:35 +0100

    reduce section header font size

--- src/components/generic/section-heading.tsx
+++ src/components/generic/section-heading.tsx
@@ -3,7 +3,9 @@ import type { TSectionHeadingProps } from "@/lib/types";
 export function SectionHeading({ children }: TSectionHeadingProps) {
   return (
     <header>
-      <h2 className="mb-8 text-3xl sm:text-4xl font-semibold">{children}</h2>
+      <h2 className="mb-8 text-2xl sm:text-3xl font-semibold uppercase">
+        {children}
+      </h2>
     </header>
   );
 }
