007be46992a88f679dd6a6b574382bf4de186f90
ionztorm <leon.codes@icloud.com>
2024-10-20 20:08:01 +0100

    remove X padding

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -2,15 +2,12 @@ import { cn } from "@/utils/cn";
 import type { TSectionProps } from "@/lib/types";
 
 export function Section({ children, id, className }: TSectionProps) {
-	return (
-		<section
-			id={id}
-			className={cn(
-				"mb-10 sm:mb-14 xl:mb-20 px-4 mx-auto max-w-5xl ",
-				className,
-			)}
-		>
-			{children}
-		</section>
-	);
+  return (
+    <section
+      id={id}
+      className={cn("mb-10 sm:mb-14 xl:mb-20 mx-auto max-w-5xl ", className)}
+    >
+      {children}
+    </section>
+  );
 }
