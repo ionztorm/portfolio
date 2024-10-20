c47534e562115bcc78eff06a3c6e6e41f5978125
ionztorm <leon.codes@icloud.com>
2024-10-20 11:26:54 +0100

    adjust wrapper sizing

--- src/components/generic/wrapper.tsx
+++ src/components/generic/wrapper.tsx
@@ -2,9 +2,7 @@ import { cn } from "@/utils/cn";
 import type { TWrapperProps } from "@/lib/types";
 
 export function Wrapper({ children, className }: TWrapperProps) {
-  return (
-    <div className={cn("w-full max-w-[70rem] mx-auto", className)}>
-      {children}
-    </div>
-  );
+	return (
+		<div className={cn("w-full max-w-5xl mx-auto", className)}>{children}</div>
+	);
 }
