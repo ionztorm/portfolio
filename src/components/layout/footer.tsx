7fae505c659d0fe007cb8e70ca851876ad7876d8
ionztorm <leon.codes@icloud.com>
2024-10-20 11:25:50 +0100

    add tech used to footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,5 +1,10 @@
-import type { TFooterProps } from "@/lib/types";
-
-export function Footer({ className }: TFooterProps) {
-  return <footer className={className}>&copy; Leon Lonsdale 2024</footer>;
+export function Footer() {
+	return (
+		<footer className="text-sm flex justify-between py-4 px-4 mx-auto max-w-5xl w-full mt-10">
+			<p>&copy; Leon Lonsdale 2024</p>
+			<p>
+				Built with <strong>NextJS</strong> and <strong>Tailwind</strong>
+			</p>
+		</footer>
+	);
 }
