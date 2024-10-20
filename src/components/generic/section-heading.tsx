60df7218f3ca3116f3cc61c27840e9d3298f9ebf
ionztorm <leon.codes@icloud.com>
2024-10-20 11:27:14 +0100

    simplify section header styles

--- src/components/generic/section-heading.tsx
+++ src/components/generic/section-heading.tsx
@@ -1,11 +1,9 @@
 import type { TSectionHeadingProps } from "@/lib/types";
 
 export function SectionHeading({ children }: TSectionHeadingProps) {
-  return (
-    <header>
-      <h2 className="text-2xl text-center lg:text-left sm:text-4xl font-bold text-slate-800 mb-10">
-        {children}
-      </h2>
-    </header>
-  );
+	return (
+		<header>
+			<h2 className="mb-8 text-3xl sm:text-4xl">{children}</h2>
+		</header>
+	);
 }
