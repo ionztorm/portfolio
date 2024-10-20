4e63d891f1117a576ed973ac995edc445b757c39
ionztorm <leon.codes@icloud.com>
2024-10-20 16:07:43 +0100

    make section headers semibold

--- src/components/generic/section-heading.tsx
+++ src/components/generic/section-heading.tsx
@@ -1,9 +1,9 @@
 import type { TSectionHeadingProps } from "@/lib/types";
 
 export function SectionHeading({ children }: TSectionHeadingProps) {
-	return (
-		<header>
-			<h2 className="mb-8 text-3xl sm:text-4xl">{children}</h2>
-		</header>
-	);
+  return (
+    <header>
+      <h2 className="mb-8 text-3xl sm:text-4xl font-semibold">{children}</h2>
+    </header>
+  );
 }
