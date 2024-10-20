5277efd9e153a8724d83174c263df7f2fed29e5e
ionztorm <leon.codes@icloud.com>
2024-10-20 16:06:47 +0100

    remove skills section

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -10,23 +10,20 @@ import { Section } from "@/components/generic/section";
 import { Skills } from "./_components/_skills/skills";
 
 export default function Home() {
-	return (
-		<div className="grid grid-rows-[auto_auto_1fr_auto]">
-			<Header className="px-4 mb-10 sm:mb-14 xl:mb-20 p-6 sm:p-14" />
-			<Main>
-				<Section id="about">
-					<About />
-				</Section>
-				<Section id="projects">
-					<Projects />
-				</Section>
-				<Section id="skills">
-					<Skills />
-				</Section>
-				<Section id="contact">
-					<Contact />
-				</Section>
-			</Main>
-		</div>
-	);
+  return (
+    <div className="grid grid-rows-[auto_auto_1fr_auto]">
+      <Header className="px-4 mb-10 sm:mb-14 xl:mb-20 p-6 sm:p-14" />
+      <Main>
+        <Section id="projects">
+          <Projects />
+        </Section>
+        <Section id="about">
+          <About />
+        </Section>
+        <Section id="contact">
+          <Contact />
+        </Section>
+      </Main>
+    </div>
+  );
 }
