e55ae655fc42778baa90175e3c3564ee35539296
ionztorm <leon.codes@icloud.com>
2024-10-21 22:35:31 +0100

    adjust skills layout variant for skills

--- src/components/ui/skill-pill.tsx
+++ src/components/ui/skill-pill.tsx
@@ -17,7 +17,7 @@ export function SkillPill({
     pillShape: {
       hero: "flex items-center flex-shrink-0 rounded-full max-w-max gap-2 px-3 py-2",
       skills:
-        "flex flex-col items-center flex-shrink-0 border rounded-2xl gap-3 min-w-32 px-2 py-4 shadow-md bg-white",
+        "flex flex-col items-center flex-shrink-0 border rounded-2xl gap-2  px-2 py-4 shadow-md bg-white min-w-36 aspect-square",
       projects:
         "flex items-center flex-shrink-0 border rounded-full max-w-max gap-1 px-3 py-1",
     },
@@ -30,7 +30,7 @@ export function SkillPill({
 
   return (
     <li key={skill.name} className={cn(variants.pillShape[variant], className)}>
-      <span style={{ color: skill.brandColour }} className="min-w-">
+      <span style={{ color: skill.brandColour }}>
         {Icon ? <Icon className={variants.iconSize[variant]} /> : null}
       </span>
       <span className={variants.textSize[variant]}>{skill.name}</span>
