f87f66de92967a959bae8071869d8322bf59341b
ionztorm <leon.codes@icloud.com>
2024-10-21 22:32:51 +0100

    update footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,10 +1,16 @@
 export function Footer() {
-	return (
-		<footer className="text-sm flex justify-between py-4 px-4 mx-auto max-w-5xl w-full mt-10">
-			<p>&copy; Leon Lonsdale 2024</p>
-			<p>
-				Built with <strong>NextJS</strong> and <strong>Tailwind</strong>
-			</p>
-		</footer>
-	);
+  return (
+    <footer className="text-sm flex justify-between items-center py-4 px-4 mx-auto max-w-5xl w-full mt-10">
+      <p>&copy; Leon Lonsdale 2024</p>
+      <div className="flex flex-col text-right">
+        <p>
+          Built with <strong>NextJS</strong>, <strong>TypeScript</strong> and{" "}
+          <strong>Tailwind</strong>
+        </p>
+        <p>
+          Powered by <strong>Vercel</strong>
+        </p>
+      </div>
+    </footer>
+  );
 }
