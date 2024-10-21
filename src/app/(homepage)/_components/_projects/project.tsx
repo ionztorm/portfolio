c85a2ad7c5e3973a0a45e2bb5dbc689325701bea
ionztorm <leon.codes@icloud.com>
2024-10-21 18:25:13 +0100

    switch to 3 col project layout and decrease card padding

--- src/app/(homepage)/_components/_projects/project.tsx
+++ src/app/(homepage)/_components/_projects/project.tsx
@@ -4,22 +4,25 @@ import Image from "next/image";
 
 export function Project({ project }: TProjectProps) {
   return (
-    <article className="border rounded-md shadow-md bg-white space-y-4">
-      <h3 className="font-semibold px-4 pt-4">{project.title}</h3>
-      <div className="px-4">
+    <article className="border rounded-md shadow-md bg-white transition duration-300 hover:scale-[1.02] hover:shadow-lg text-sm">
+      <h3 className="font-semibold px-2 py-2">{project.title}</h3>
+      <div className="px-2">
         <Image
           src={project.image}
           alt="VS Code Theme"
           className="rounded-md overflow-hidden aspect-video object-cover"
         />
       </div>
-      <p className="px-4">{project.overview}</p>
-      <ul className="border-t p-4 flex gap-2 flex-wrap">
+      <p className="px-2 py-2">{project.overview}</p>
+      <div className="border-t flex w-full justify-end gap-1 p-2 items-center">
+        <p>links</p>
+      </div>
+      <ul className="border-t p-2 flex gap-1 flex-wrap">
         {Object.values(project.skills).map((skill) => (
           <SkillPill
             key={skill.name}
             skill={skill}
-            className="px-2 py-1 gap-1"
+            className="py-1 px-2 gap-1"
             kind="projects"
           />
         ))}
