318eef97c80f2d447a84ed17983b718731a7653c
ionztorm <leon.codes@icloud.com>
2024-10-21 14:31:28 +0100

    change hero text

--- src/app/(homepage)/_components/_hero/hero.tsx
+++ src/app/(homepage)/_components/_hero/hero.tsx
@@ -1,13 +1,17 @@
 export function Hero() {
   return (
-    <h1 className="group grid gap-2 text-center">
-      <span className="text-center text-sm">Junior</span>
+    <h1 className="group grid gap-1 text-center">
+      <span className="text-center text-sm">I&apos;m a</span>
 
-      <span className="text-3xl sm:text-5xl font-bold">
+      <span className="text-3xl sm:text-5xl font-bold text-[#2ac3de]">
         Fullstack Web Developer
       </span>
       {/* Underline Span */}
       <span className="h-1 bg-violet-500 w-0 transition-all duration-500 group-hover:w-full" />
+
+      <span className="text-center text-sm">
+        with a passion for clean, responsive, data driven interfaces.
+      </span>
     </h1>
   );
 }
