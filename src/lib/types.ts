d74d4e2f412c50e4a1b40c9eaaee431833453acf
ionztorm <leon.codes@icloud.com>
2024-10-22 19:49:50 +0100

    organise types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,6 +3,14 @@ import type { StaticImageData } from "next/image";
 import type { PropsWithChildren } from "react";
 import type { IconType } from "react-icons";
 
+// react types
+
+export type TChildrenProps = Readonly<PropsWithChildren>;
+
+// tailwind types
+
+export type TScreenSizes = keyof typeof twScreens;
+
 // paths
 
 export type TPath = { path: string; label: string; icon?: string };
@@ -40,31 +48,10 @@ export type TProject = {
   links?: TPaths;
 };
 export type TProjects = TProject[];
-
-// react types
-
-export type TChildrenProps = Readonly<PropsWithChildren>;
-
-// tailwind types
-
-export type TScreenSizes = keyof typeof twScreens;
-
-// component props
-
 export type TProjectProps = Readonly<{ project: TProject }>;
-export type TIconProps = Readonly<{ className?: string }>;
-export type TLogoProps = Readonly<{ title: string; subtext?: string }>;
-export type TSectionHeadingProps = TChildrenProps;
-export type TSectionProps = Readonly<{
-  id?: string;
-  heading?: string;
-  className?: string;
-}> &
-  TChildrenProps;
-export type TWrapperProps = Readonly<{ className?: string }> & TChildrenProps;
-export type THeaderProps = Readonly<{ className?: string }>;
-export type TFooterProps = Readonly<{ className?: string }>;
-export type TMainProps = Readonly<{ className?: string }> & TChildrenProps;
+
+// navbar
+
 export type TNavbarProps = Readonly<{ paths: TPaths }> & TChildrenProps;
 export type TNavLinkProps = Readonly<{
   path: TPath["path"];
@@ -72,3 +59,24 @@ export type TNavLinkProps = Readonly<{
   onClick?: () => void;
 }> &
   TChildrenProps;
+
+// layout components
+
+export type THeaderProps = Readonly<{ className?: string }>;
+export type TFooterProps = Readonly<{ className?: string }>;
+export type TMainProps = Readonly<{ className?: string }> & TChildrenProps;
+
+export type TSectionHeadingProps = TChildrenProps;
+export type TSectionProps = Readonly<{
+  id?: string;
+  heading?: string;
+  className?: string;
+}> &
+  TChildrenProps;
+
+export type TWrapperProps = Readonly<{ className?: string }> & TChildrenProps;
+
+// ui component props
+
+export type TIconProps = Readonly<{ className?: string }>;
+export type TLogoProps = Readonly<{ title: string; subtext?: string }>;
