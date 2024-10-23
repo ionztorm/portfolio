cc904256ddb74aa6299b9760299deae2d708ceb1
ionztorm <leon.codes@icloud.com>
2024-10-23 21:57:10 +0100

    increase text sizes for readability

--- src/app/(homepage)/_components/_hero/hero.tsx
+++ src/app/(homepage)/_components/_hero/hero.tsx
@@ -1,7 +1,7 @@
 export function Hero() {
   return (
     <h1 className="group grid gap-1 text-center">
-      <span className="text-center text-sm">I&apos;m a</span>
+      <span className="text-center">I&apos;m a</span>
 
       <span className="text-3xl sm:text-5xl font-bold text-[#6737B9]">
         Fullstack Web Developer
@@ -9,7 +9,7 @@ export function Hero() {
       {/* Underline Span */}
       <span className="h-1 bg-[#73daca] w-0 transition-all duration-500 group-hover:w-full" />
 
-      <span className="text-center text-sm">
+      <span className="text-center">
         with a passion for clean, responsive, data driven interfaces
       </span>
     </h1>
