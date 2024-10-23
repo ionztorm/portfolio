fb036325e0f6b1ee760762911ba0858b32311d35
ionztorm <leon.codes@icloud.com>
2024-10-23 16:17:52 +0100

    make visible against new nav bg colour

--- src/components/ui/burger-button.tsx
+++ src/components/ui/burger-button.tsx
@@ -14,7 +14,7 @@ export function BurgerButton({ onClick, isOpen }: TBurgerButtonProps) {
       aria-expanded={isOpen}
     >
       <div
-        className={`w-6 h-0.5 bg-slate-900 transition-transform duration-300 ease-in-out before:content-[''] before:block before:absolute before:w-6 before:h-0.5 before:bg-slate-900 before:transition-transform before:duration-300 before:ease-in-out after:content-[''] after:block after:absolute after:w-6 after:h-0.5 after:bg-slate-900 after:transition-transform after:duration-300 after:ease-in-out ${
+        className={`w-6 h-0.5 bg-slate-50 transition-transform duration-300 ease-in-out before:content-[''] before:block before:absolute before:w-6 before:h-0.5 before:bg-slate-50 before:transition-transform before:duration-300 before:ease-in-out after:content-[''] after:block after:absolute after:w-6 after:h-0.5 after:bg-slate-50 after:transition-transform after:duration-300 after:ease-in-out ${
           isOpen
             ? "rotate-45 before:rotate-90 before:-translate-y-0 after:-rotate-90 after:-translate-y-0"
             : "before:-translate-y-2 after:translate-y-2"
