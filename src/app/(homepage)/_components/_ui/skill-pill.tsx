acc72844b8bd9a01db19697d9a9870ee2634fc28
ionztorm <leon.codes@icloud.com>
2024-10-23 16:26:18 +0100

    smaller skill pills on mobile

--- src/app/(homepage)/_components/_ui/skill-pill.tsx
+++ src/app/(homepage)/_components/_ui/skill-pill.tsx
@@ -11,19 +11,19 @@ export function SkillPill({
   const variants = {
     iconSize: {
       hero: "size-6",
-      skills: "size-16",
+      skills: "size-8 sm:size-16",
       projects: "size-4",
     },
     pillShape: {
       hero: "flex items-center flex-shrink-0 border rounded-full max-w-max gap-2 px-3 py-2 bg-white shadow-sm",
       skills:
-        "flex flex-col items-center flex-shrink-0 border rounded-2xl gap-2  px-2 py-4 shadow-md bg-white min-w-36 aspect-square",
+        "flex flex-col items-center flex-shrink-0 border rounded-2xl gap-2  px-2 py-4 shadow-md bg-white min-w-24 sm:min-w-36 aspect-square",
       projects:
         "flex items-center flex-shrink-0 border rounded-full max-w-max gap-1 px-3 py-1",
     },
     textSize: {
       hero: "text-base md:text-md xl:text-lg",
-      skills: "text-base md:text-md xl:text-lg",
+      skills: "text-xs md:text-md xl:text-lg",
       projects: "text-xs",
     },
   };
