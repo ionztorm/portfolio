568827472ed167d006345585e8c7aeeb86b3dd70
ionztorm <leon.codes@icloud.com>
2024-10-23 16:21:43 +0100

    reword and add about section back into page

--- src/app/(homepage)/_components/_about/about.tsx
+++ src/app/(homepage)/_components/_about/about.tsx
@@ -11,19 +11,24 @@ export function About() {
           Hey, I&apos;m Leon. I&apos;m a <strong>fullstack</strong> web
           developer with a <strong>passion</strong> for <strong>modern</strong>{" "}
           technologies and developing <strong>clean</strong>,{" "}
-          <strong>responsive user experiences</strong>.
+          <strong>responsive user experiences</strong>. I&apos;m also bald -
+          this is very important information.
         </p>
         <p>
-          I&apos;m self taught, which has allowed me to choose the technologies
-          I have a passion for. Check out my{" "}
-          <StyledLink path={HOMEPAGE_NAV_PATHS.skills} lowerCase={true} /> below
-          to see what I&apos;ve been spending my time on.
+          Being self taught has allowed me to pursue the technologies that I
+          find interesting - and I&apos;m not finished yet! You can check out
+          the topic I&apos; learned and used in my{" "}
+          <StyledLink path={HOMEPAGE_NAV_PATHS.skills} lowerCase={true} />{" "}
+          section.
         </p>
         <p>
-          When I&apos;m not studying, I enjoy watching Formula 1, and have a
-          collection of model F1 cars and helmets (15 cars and 3 helmets, and
-          counting). I like to keep myself active with either a 25km indoor
-          cycle or 6km outdoor walk every day, weather permitting.
+          I&apos;m an avid Formula 1 fan and will sink my spare time into the TV
+          whenever it&apos;s on and my head isn&apos; in the books, and I
+          collect models of the cars and helmets too 🤓. But I can&apos;t spend
+          all my time sitting in a seat looking at screens, so I like to keep
+          myself active with either a 25km indoor cycle or 6km outdoor walk
+          every day, weather permitting - but, as I say, I&apos;m bald, so the
+          rain doesn&apos; bother me much.
         </p>
       </article>
     </>
--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -6,21 +6,23 @@ import { Projects } from "@/app/(homepage)/_components/_projects/projects";
 
 import { Section } from "@/components/ui/section";
 import { Skills } from "@/app/(homepage)//_components/_skills/skills";
+import { About } from "./_components/_about/about";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr]">
       <Header />
       <Main>
+        <Section id="about">
+          <About />
+        </Section>
         <Section id="projects">
           <Projects />
         </Section>
         <Section id="skills">
           <Skills />
         </Section>
-        {/* <Section id="about">
-          <About />
-        </Section>
+        {/* 
         <Section id="contact">
           <Contact />
         </Section> */}
