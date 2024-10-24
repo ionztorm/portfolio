befceab9a927720d6560dca1aca7ad9db5ce49eb
ionztorm <leon.codes@icloud.com>
2024-10-24 21:05:48 +0100

    create profile component

---
+++ src/app/(cv)/cv/_components/_profile/profile.tsx
@@ -0,0 +1,10 @@
+import { SectionHeading } from "@/components/ui/section-heading";
+
+export function Profile() {
+  return (
+    <>
+      <SectionHeading>Profile</SectionHeading>
+      <p>Some stuff about me</p>
+    </>
+  );
+}
