4001c7174daf2c652d012f8a97cec697bf92e523
ionztorm <leon.codes@icloud.com>
2024-10-24 21:03:02 +0100

    fix href in logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import type { TLogoProps } from "@/lib/types";
 
 export function Logo({ title, subtext }: TLogoProps) {
-	return (
-		<p className="grid">
-			<Link href="#">
-				<span className="text-xl md:text-2xl font-bold">{title}</span>
-			</Link>
-			{subtext && <span>{subtext}</span>}
-		</p>
-	);
+  return (
+    <p className="grid">
+      <Link href="/">
+        <span className="text-xl md:text-2xl font-bold">{title}</span>
+      </Link>
+      {subtext && <span>{subtext}</span>}
+    </p>
+  );
 }
